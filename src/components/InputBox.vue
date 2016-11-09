b38a2ca2604370826c26ad682d53d4f42ac46e06
kemalkautsar <kemal.kautsar@gmail.com>
2016-11-09 08:27:07 +0700

    initial files

---
+++ src/components/InputBox.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" v-model="todo">
+    <button @click="add()">Add todo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'inputBox',
+  data () {
+    return {
+      todo: ''
+    }
+  },
+  methods: {
+    add () {
+      this.$parent.items.push(this.todo)
+      this.todo = ''
+    }
+  }
+}
+</script>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in items">{{item}} <a class="remove" @click="remove(index)">remove</a></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'listItem',
+  props: {
+    items: {
+      required: true
+    }
+  },
+  methods: {
+    remove (index) {
+      this.items.splice(index, 1)
+      console.log(index)
+    }
+  }
+}
+</script>
